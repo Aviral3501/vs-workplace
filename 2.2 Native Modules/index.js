e9be96489442222d9fdea239773514c1f876fbbe
Aviral Singh <aviralsingh3501@gmail.com>
2023-07-27 14:28:24 +0530

    reading a file from the fs using node

--- 2.2 Native Modules/index.js
+++ 2.2 Native Modules/index.js
@@ -1,5 +1,6 @@
 const fs = require("fs");// fs is short for file sysytem
-fs.writeFile("message1.txt","Hello1 from node js",(err)=>{
-    if(err) throw err;
-    console.log("the file has been saved");
+fs.readFile("./message.txt","utf8", (err , data)=>{
+    if (err) throw err;
+    console.log(data);
 });
+// reason we added "utf8" is because if we read the file then we get default encoded form. hence we apply encoding  "utf8" to the file to read it
