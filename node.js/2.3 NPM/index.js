314148df10e3fd8a4790e32680ddd67cedf8755f
Aviral Singh <aviralsingh3501@gmail.com>
2023-07-28 00:04:40 +0530

    using esm mpdules for the sillyname projects

--- node.js/2.3 NPM/index.js
+++ node.js/2.3 NPM/index.js
@@ -1,4 +1,10 @@
-var generateName = require("sillyname");
+// var generateName = require("sillyname");
+// var sillyName = generateName();
+//this method is by using CJS (common js )
+
+// another way of doing it is by using ESM (ecma script modules)
+
+import generateName from "sillyname";
 var sillyName = generateName();
 
 // console.log("My name is "+ sillyName+"." );
