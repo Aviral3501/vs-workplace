7510dbd6d1ddd0b45a9aa4d1734c4f819421a678
Aviral Singh <aviralsingh3501@gmail.com>
2023-07-28 00:25:30 +0530

    using superheroes pachage to create a name

--- node.js/2.3 NPM/index.js
+++ node.js/2.3 NPM/index.js
@@ -4,8 +4,14 @@
 
 // another way of doing it is by using ESM (ecma script modules)
 
-import generateName from "sillyname";
-var sillyName = generateName();
+// import generateName from "sillyname";
+// var sillyName = generateName();
 
 // console.log("My name is "+ sillyName+"." );
-console.log(`My name is ${sillyName}.`);
+// console.log(`My name is ${sillyName}.`);
+
+import superheroes from "superheroes";
+const name = superheroes.random();
+console.log("I am "+ name);
+
+// import xxx(i.e. methddname) from "xxx(i.e. package name)" ;
