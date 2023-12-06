0d0df1ac962918923f1da19b75916048ba94830f
Aviral Singh <aviralsingh3501@gmail.com>
2023-12-06 10:59:41 +0530

    commit

--- jQuery/index.js
+++
@@ -1,8 +0,0 @@
-//document.querySelectorAll("button")
-// $("h1").css("color", "green");
-$("h1").on("mouseover", function(){
-    $(this).css("color","purple");
-    setTimeout(function(){
-    $("h1").css("color","black");
-    },500);
-});
--- node.js/2.2 Native Modules/index.js
+++
@@ -1,6 +0,0 @@
-const fs = require("fs");// fs is short for file sysytem
-fs.readFile("./message.txt","utf8", (err , data)=>{
-    if (err) throw err;
-    console.log(data);
-});
-// reason we added "utf8" is because if we read the file then we get default encoded form. hence we apply encoding  "utf8" to the file to read it
